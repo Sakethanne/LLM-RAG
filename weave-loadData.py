347e6c598f2e62ef228be65f2ab29c7d600c21c9
sakethanne <36626206+Sakethanne@users.noreply.github.com>
2024-04-28 15:50:31 -0700

    Final commit

---
+++ weave-loadData.py
@@ -0,0 +1,47 @@
+from weaviate import *
+import json
+import warnings
+
+# Suppress DeprecationWarning
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+client = Client(
+    url = "http://localhost:8080/",  
+)
+
+
+
+
+# specify schema for the data we'll be using 
+
+client.schema.delete_class("SimSearch") 
+
+class_obj = {
+    "class": "SimSearch",
+    "vectorizer": "text2vec-transformers"
+}
+client.schema.create_class(class_obj)
+
+
+# download data
+import requests
+url = 'http://localhost:8000/data.json'
+resp = requests.get(url)
+data = json.loads(resp.text)
+
+# send data to weaviate, to vectorize
+with client.batch as batch:
+    batch.batch_size=100
+    # Batch import all data
+    for i, d in enumerate(data):
+        print(f"\nimporting datum: {i}")
+
+        properties = {
+            "author": d["Author"],
+            "book": d["Book"],
+            "summary": d["Summary"],
+        }
+        print(f"properties: {properties}")
+
+        client.batch.add_data_object(properties, "SimSearch")
